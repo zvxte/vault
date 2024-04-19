851eb6eb4717b82522e72d629935856f4cfbc834
zvxte <zvxte.codes@gmail.com>
2024-04-19 22:35:55 +0200

    add server binary crate

---
+++ server-bin/src/main.rs
@@ -0,0 +1,15 @@
+use tokio::net::TcpListener;
+use axum::Router;
+use server_lib::routers;
+
+#[tokio::main]
+async fn main() {
+    let users_router = routers::users_router();
+    let app = Router::new()
+    .nest("/users", users_router);
+
+    let address = "127.0.0.1:5000";
+    let listener = TcpListener::bind(address).await.unwrap();
+    
+    axum::serve(listener, app).await.unwrap();
+}
