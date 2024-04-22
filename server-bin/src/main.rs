d69256914f57ae2f2febf6472878f536f277f532
zvxte <zvxte.codes@gmail.com>
2024-04-22 22:46:43 +0200

    add await to users router

--- server-bin/src/main.rs
+++ server-bin/src/main.rs
@@ -6,7 +6,7 @@ use server_lib::routers;
 async fn main() {
     let users_router = routers::users_router();
     let app = Router::new()
-        .nest("/users", users_router);
+        .nest("/users", users_router.await);
 
     let address = "127.0.0.1:5000";
     let listener = TcpListener::bind(address).await.unwrap();
