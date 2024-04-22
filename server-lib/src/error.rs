125e124e4fb2eb94646e2de82733b7d43b433a82
zvxte <zvxte.codes@gmail.com>
2024-04-22 22:48:24 +0200

    add custom error type

---
+++ server-lib/src/error.rs
@@ -0,0 +1,20 @@
+use sqlx;
+
+#[derive(Debug)]
+pub enum Error {
+    DatabaseError,
+    SqlxError(sqlx::Error),
+    SqlxMigrateError(sqlx::migrate::MigrateError),
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(value: sqlx::Error) -> Self {
+        Error::SqlxError(value)
+    }
+}
+
+impl From<sqlx::migrate::MigrateError> for Error {
+    fn from(value: sqlx::migrate::MigrateError) -> Self {
+        Error::SqlxMigrateError(value)
+    }
+}
