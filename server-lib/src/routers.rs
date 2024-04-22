82f856521c58aa67bd936c9dea3fc78d685e3777
zvxte <zvxte.codes@gmail.com>
2024-04-22 22:56:12 +0200

    add database state

--- server-lib/src/routers.rs
+++ server-lib/src/routers.rs
@@ -1,13 +1,27 @@
+use std::env;
 use axum::{
     routing::post,
     Router,
 };
 use crate::routes::users;
+use crate::database::PostgreDb;
 use crypto::Argon2Hasher;
 
-pub fn users_router() -> Router {
-    let argon2_state = Argon2Hasher::new();
+#[derive(Clone)]
+pub struct AppState<'a> {
+    pub hasher: Argon2Hasher<'a>,
+    pub database: PostgreDb,
+}
+
+pub async fn users_router() -> Router {
+    let app_state = AppState {
+        hasher: Argon2Hasher::new(),
+        database: PostgreDb::build(
+            env::var("DATABASE_URL").expect("DATABASE_URL not set")
+        ).await.expect("Invalid database configuration"),
+    };
     Router::new()
         .route("/register", post(users::post_users_register))
-        .with_state(argon2_state)
+        .route("/login", post(users::post_users_login))
+        .with_state(app_state)
 }
