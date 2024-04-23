63ed8196d6ca66bb40d3140a58fa6b781ebd8b66
zvxte <zvxte.codes@gmail.com>
2024-04-23 23:49:16 +0200

    add passwords router with middleware to validate session id

--- server-lib/src/routers.rs
+++ server-lib/src/routers.rs
@@ -3,9 +3,10 @@ use axum::{
     routing::post,
     Router,
 };
-use crate::routes::users;
-use crate::database::PostgreDb;
 use crypto::Argon2Hasher;
+use crate::routes::{users, passwords};
+use crate::database::PostgreDb;
+use crate::middleware;
 
 #[derive(Clone)]
 pub struct AppState<'a> {
@@ -23,5 +24,18 @@ pub async fn users_router() -> Router {
     Router::new()
         .route("/register", post(users::post_users_register))
         .route("/login", post(users::post_users_login))
+        .with_state(app_state)
+}
+
+pub async fn passwords_router() -> Router {
+    let app_state = AppState {
+        hasher: Argon2Hasher::new(),
+        database: PostgreDb::build(
+            env::var("DATABASE_URL").expect("DATABASE_URL not set")
+        ).await.expect("Invalid database configuration"),
+    };
+    Router::new()
+        .route("/", post(passwords::post_passwords))
+        .route_layer(axum::middleware::from_fn_with_state(app_state.clone(), middleware::validate_session))
         .with_state(app_state)
 }
