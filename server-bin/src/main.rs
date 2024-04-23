f0c6a5e8f9d38079f32222aeeb8f79500900013b
zvxte <zvxte.codes@gmail.com>
2024-04-23 23:46:30 +0200

    add passwords router

--- server-bin/src/main.rs
+++ server-bin/src/main.rs
@@ -5,8 +5,10 @@ use server_lib::routers;
 #[tokio::main]
 async fn main() {
     let users_router = routers::users_router();
+    let passwords_router = routers::passwords_router();
     let app = Router::new()
-        .nest("/users", users_router.await);
+        .nest("/users", users_router.await)
+        .nest("/passwords", passwords_router.await);
 
     let address = "127.0.0.1:5000";
     let listener = TcpListener::bind(address).await.unwrap();
