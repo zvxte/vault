774bf3e26eeb66055a127bbb6a67ad990cfd0c6f
zvxte <zvxte.codes@gmail.com>
2024-04-23 23:57:40 +0200

    minimize code duplication

--- server-lib/src/routes/users.rs
+++ server-lib/src/routes/users.rs
@@ -1,14 +1,12 @@
 use axum::{
-    extract::{
-        rejection::JsonRejection,
-        Json, State,
-    },
+    extract::{rejection::JsonRejection, Json, State},
     http::StatusCode,
-    response::{Response, IntoResponse},
+    response::{IntoResponse, Response},
 };
+
 use crypto::Hasher;
-use crate::routes::model::{DataResponse, MessageResponse, UserIn, UserOut};
 use crate::database::Db;
+use crate::model::{MessageResponse, UserIn, UserOut};
 use crate::routers::AppState;
 
 pub async fn post_users_register<'a>(
@@ -16,42 +14,20 @@ pub async fn post_users_register<'a>(
     user: Result<Json<UserIn>, JsonRejection>,
 ) -> Response {
     let user = match user {
-        Ok(user) => {
-            user.0
-        },
-        Err(err) => {
-            return MessageResponse::new(
-                StatusCode::BAD_REQUEST,
-                err.to_string(),
-            ).into_response()
-        }
+        Ok(user) => user.0,
+        Err(err) => return MessageResponse::bad_request(err.to_string()),
     };
+
     let hasher = &state.hasher;
-    
     let hashed_password = match hasher.hash_password(&user.password) {
         Ok(pwd) => pwd,
-        Err(_) => {
-            return MessageResponse::new(
-                StatusCode::BAD_REQUEST,
-                "Failed to register a new account".to_string(),
-            ).into_response()
-        }
+        Err(_) => return MessageResponse::bad_request("Failed to register a new account".to_string()),
     };
-    
+
     let database = &state.database;
     match database.create_user(&user.name, &hashed_password).await {
-        Ok(_) => {
-            MessageResponse::new(
-                StatusCode::CREATED,
-                "Account created".to_string(),
-            ).into_response()
-        },
-        Err(_) => {
-            MessageResponse::new(
-                StatusCode::BAD_REQUEST,
-                "Failed to register a new account".to_string(),
-            ).into_response()
-        }
+        Ok(_) => MessageResponse::created("Account created".to_string()),
+        Err(_) => MessageResponse::bad_request("Failed to register a new account".to_string()),
     }
 }
 
@@ -60,40 +36,34 @@ pub async fn post_users_login<'a>(
     user: Result<Json<UserIn>, JsonRejection>,
 ) -> Response {
     let user = match user {
-        Ok(user) => {
-            user.0
-        },
-        Err(err) => {
-            return MessageResponse::new(
-                StatusCode::BAD_REQUEST,
-                err.to_string(),
-            ).into_response()
-        }
+        Ok(user) => user.0,
+        Err(err) => return MessageResponse::bad_request(err.to_string()),
     };
 
     let database = &state.database;
     let dbuser = match database.check_user(&user.name).await {
         Ok(dbuser) => dbuser,
-        Err(_) => return {
-            MessageResponse::new(
-                StatusCode::BAD_REQUEST,
-                "Failed to login".to_string(),
-            ).into_response()
-        }
+        Err(_) => return MessageResponse::bad_request("Failed to login".to_string()),
     };
 
     let hasher = &state.hasher;
-    if let Ok(result) = hasher.cmp_password(&user.password,&dbuser.password) {
+    if let Ok(result) = hasher.cmp_password(&user.password, &dbuser.password) {
         if result {
-            return DataResponse::new(
+            let session_id = match database.create_session(&dbuser.user_id).await {
+                Ok(session_id) => session_id,
+                Err(_) => return MessageResponse::bad_request("Failed to login".to_string()),
+            };
+
+            return (
                 StatusCode::OK,
-                UserOut::from_dbuser(dbuser),
-            ).into_response()
+                [
+                    ("Content-Type", "application/json"),
+                    ("session_id", &session_id),
+                ],
+                Json(UserOut::from_dbuser(dbuser)),
+            ).into_response();
         }
     }
 
-    MessageResponse::new(
-        StatusCode::BAD_REQUEST,
-        "Failed to login".to_string(),
-    ).into_response()
+    MessageResponse::bad_request("Failed to login".to_string())
 }
