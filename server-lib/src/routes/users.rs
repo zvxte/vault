8a430718e81e180bd1ed00a609fbc6254a442533
zvxte <zvxte.codes@gmail.com>
2024-04-23 11:49:22 +0200

    move shared structures to seperate file

--- server-lib/src/routes/users.rs
+++ server-lib/src/routes/users.rs
@@ -4,85 +4,12 @@ use axum::{
         Json, State,
     },
     http::StatusCode,
-    response::Response,
-    response::IntoResponse,
+    response::{Response, IntoResponse},
 };
-use serde::{Deserialize, Serialize};
 use crypto::Hasher;
-use crate::database::{Db, DbUser};
+use crate::routes::model::{DataResponse, MessageResponse, UserIn, UserOut};
+use crate::database::Db;
 use crate::routers::AppState;
-
-const CONTENT_TYPE_JSON: [(&str, &str); 1] = [("Content-Type", "application/json")];
-
-#[derive(Serialize)]
-pub struct DataResponse<T: Serialize> {
-    #[serde(skip)]
-    status_code: StatusCode,
-    #[serde(flatten)]
-    data: T,
-}
-
-impl<T: Serialize> DataResponse<T> {
-    fn new(status_code: StatusCode, data: T) -> Self {
-        Self { status_code, data }
-    }
-}
-
-impl<T: Serialize> IntoResponse for DataResponse<T> {
-    fn into_response(self) -> Response {
-        (
-            self.status_code,
-            CONTENT_TYPE_JSON,
-            Json(self.data),
-        ).into_response()
-    }
-}
-
-#[derive(Serialize)]
-pub struct MessageResponse {
-    #[serde(skip)]
-    status_code: StatusCode,
-    message: String,
-}
-
-impl MessageResponse {
-    fn new(status_code: StatusCode, message: String) -> Self {
-        Self { status_code, message }
-    }
-}
-
-impl IntoResponse for MessageResponse {
-    fn into_response(self) -> Response {
-        (
-            self.status_code,
-            CONTENT_TYPE_JSON,
-            Json(self),
-        ).into_response()
-    }
-}
-
-#[derive(Deserialize)]
-pub struct UserIn {
-    name: String,
-    password: String,
-}
-
-#[derive(Serialize)]
-pub struct UserOut {
-    user_id: String,
-    username: String,
-    salt: [u8; 32],
-}
-
-impl UserOut {
-    fn from_dbuser(dbuser: DbUser) -> Self {
-        Self {
-            user_id: dbuser.user_id.to_string(),
-            username: dbuser.username,
-            salt: dbuser.salt
-        }
-    }
-}
 
 pub async fn post_users_register<'a>(
     State(state): State<AppState<'a>>,
