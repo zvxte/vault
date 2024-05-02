28a183445b37ac148bc16b154ef429a3864128c6
zvxte <zvxte.codes@gmail.com>
2024-05-02 13:48:03 +0200

    add notes router

--- server-bin/src/main.rs
+++ server-bin/src/main.rs
@@ -6,9 +6,11 @@ use tokio::net::TcpListener;
 async fn main() {
     let users_router = routers::users_router();
     let passwords_router = routers::passwords_router();
+    let notes_router = routers::notes_router();
     let app = Router::new()
         .nest("/users", users_router.await)
-        .nest("/passwords", passwords_router.await);
+        .nest("/passwords", passwords_router.await)
+        .nest("/notes", notes_router.await);
 
     let address = "127.0.0.1:5000";
     let listener = TcpListener::bind(address).await.unwrap();
